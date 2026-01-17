4d33c0cbe5c02cb06ed9c533db89089fb47859c6
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-17 20:03:06 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Analytics } from '@vercel/analytics/next'
-import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -46,7 +45,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <head />
       <body className="bg-black text-white">
         {children}
